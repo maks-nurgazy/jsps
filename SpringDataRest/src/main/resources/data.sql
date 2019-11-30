045c603e9b477b76905cb7772af239199798747e
maks-nurgazy <maksatbek.bolushov@gmail.com>
2019-11-30 15:39:05 +0600

    All in one code --RepositoryRestResource

---
+++ SpringDataRest/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into alien values (101,'Maksatbek','Java');
+insert into alien values (102,'Beknur','React Native');
+insert into alien values (103,'Musurmanbek','Php');
+insert into alien values (104,'Turar','Java');
+insert into alien values (105,'Islambek','Php');
