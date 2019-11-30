b240ef10488dbb8e6403620a6f00ff869d3f1d02
maks-nurgazy <maksatbek.bolushov@gmail.com>
2019-11-30 14:47:16 +0600

    Requestbody in POstMapping we used because we are sending raw data

--- bootjpa/src/main/java/com/telusko/demo/AlienController.java
+++ bootjpa/src/main/java/com/telusko/demo/AlienController.java
@@ -4,15 +4,12 @@ import com.telusko.demo.dao.AlienRepo;
 import com.telusko.demo.model.Alien;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
-@Controller
+@RestController
 public class AlienController {
 
     @Autowired
@@ -23,19 +20,17 @@ public class AlienController {
         return "test.jsp";
     }
 
-    @RequestMapping(value = "/addAlien",method = {RequestMethod.POST})
-    public String addAlien(Alien alien){
+    @PostMapping("alien")
+    public Alien addAlien(@RequestBody Alien alien){
         repo.save(alien);
-        return "test.jsp";
+        return alien;
     }
 
-    @RequestMapping(path = "/aliens")
-    @ResponseBody
+    @GetMapping(path = "/aliens")
     public List<Alien> getAliens(){
         return repo.findAll();
     }
     @RequestMapping("/alien/{maks}")
-    @ResponseBody
     public Optional<Alien> getAlien(@PathVariable("maks") int maks){
         return repo.findById(maks);
     }
